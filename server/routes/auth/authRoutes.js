54755e86d2903e4f665045bd4e4019f95cf03212
chaya41182 <chaya41182@gmail.com>
2024-03-31 00:07:53 +0300

    Change logout route to use POST method

--- server/routes/auth/authRoutes.js
+++ server/routes/auth/authRoutes.js
@@ -5,6 +5,6 @@ const authController = require('../../controllers/authControllers/authController
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/refresh', authController.refresh);
-router.get('/logout', authController.logout);
+router.post('/logout', authController.logout);
 
 module.exports = router;
